9453b28bf3118ae3a4289bda5d8aff083832300d
Lokesh-04 <98595361+Lokesh-04@users.noreply.github.com>
2023-11-05 19:08:57 +0530

    made some changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
-import {React,useState} from "react";
-import {BrowserRouter as Router,Route,Routes,BrowserRouter,Link} from "react-router-dom";
+import {React} from "react";
+import {BrowserRouter as Router,Route,Routes,Link} from "react-router-dom";
 import Home from "./components/Home";
 import Input from "./components/Input";
 import Output from "./components/Output";
 
 const App = () => {  
-
   return (
     <div>
       <Router>
-
       <Link to="/" style={{margin: '10px' }} >Home</Link>
       <Link to="/input" style={{margin: '10px' }} >Input</Link>
       <Link to="/output" style={{margin: '10px' }} >Output</Link>
@@ -19,7 +17,6 @@ const App = () => {
         <Route path="/input" element={<Input/>} />
         <Route path="/output" element={<Output/>} />
       </Routes>
-      
       </Router>
     </div>
   );
