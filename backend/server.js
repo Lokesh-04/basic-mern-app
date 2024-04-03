07b751a7dda85ece16d941655de7166386cab144
Lokesh-04 <98595361+Lokesh-04@users.noreply.github.com>
2024-04-03 19:10:27 +0530

    corrected

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,22 @@ import mongoose from "mongoose";
 import Data from "./models/dataModel.js"; //importing model
 import cors from "cors";
 import dotenv from "dotenv";
+dotenv.config();
 
 const app = express(); // creates a http server
 const port = 3000;
 
 mongoose.connect(process.env.MONGO_URI); // mongoDB connection string
 // mongodb://localhost:27017/mern
-app.use(cors(
+app.use(
+  cors()
   // {origin : "https://test-app-frontend-delta.vercel.app",
   //   methods : ["GET","POST","PUT","DELETE"],
   //   credentials : true}
-    ));
+);
 app.use(express.json());
-app.use(dotenv());
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
   res.json({
     h1Text: "Home page which is rendered from backend",
   });
@@ -29,25 +30,25 @@ app.get("/input", (req, res) => {
   });
 });
 
-app.post("/input", async(req, res) => {
-  const count = await Data.countDocuments({}) + 1;
+app.post("/input", async (req, res) => {
+  const count = (await Data.countDocuments({})) + 1;
   const data = new Data({
     id: count,
     data: req.body.data,
   });
-  
+
   await data.save();
 });
 
 app.get("/output", async (req, res) => {
   try {
-     const data = await Data.find({});
-     res.json(data);
+    const data = await Data.find({});
+    res.json(data);
   } catch (error) {
-     console.error("Error fetching data:", error);
-     res.status(500).json({ error: "An error occurred while fetching data." });
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "An error occurred while fetching data." });
   }
- });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
